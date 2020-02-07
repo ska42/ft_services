2d4e062bc651221a2835164b97c80eb4a5300d43
SimplySka <skalyxe00@gmail.com>
2020-02-07 19:58:37 +0100

    add config & user/password not working but added

--- srcs/ftps/srcs/install.sh
+++ srcs/ftps/srcs/install.sh
@@ -4,29 +4,10 @@
 apk add openrc
 apk add vsftpd
 
-mkdir -p /home/ftps
-chown -R nobody:nogroup /home/ftps
-chmod -R a-w /home/ftps
-mkdir /home/ftps/writeable
-chmod 777 /home/ftps/writeable
-echo "anon" | tee /home/ftps/anon.txt
+mkdir -p /var/empty
+mkdir -p /home/ftp
 
-cat <<EOF | tee /etc/vsftpd/vsftpd.conf
-anon_mkdir_write_enable=YES
-anon_other_write_enable=YES
-anon_root=/home/ftps
-anon_upload_enable=YES
-anon_world_readable_only=YES
-anonymous_enable=YES
-ascii_upload_enable=YES
-dirmessage_enable=YES
-listen=YES
-local_enable=YES
-ssl_enable=NO
-syslog_enable=YES
-write_enable=YES
-seccomp_sandbox=NO
-EOF
+chown nodoby:nogroup /home/ftp
 
 openrc
 touch /run/openrc/softlevel
