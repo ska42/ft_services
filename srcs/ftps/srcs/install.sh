a1e9d41e5033fb47d5d6dd36c8641735f3654808
SimplySka <skalyxe00@gmail.com>
2020-02-07 20:21:41 +0100

    user ssh

--- srcs/ftps/srcs/install.sh
+++ srcs/ftps/srcs/install.sh
@@ -4,11 +4,12 @@
 apk add openrc
 apk add vsftpd
 
+# create ftp's folders
 mkdir -p /var/empty
 mkdir -p /home/ftp
-
 chown nodoby:nogroup /home/ftp
 
+# userlist
 echo "$FTP_USERNAME\n$FTP_PASSWORD" >> /etc/vsftpd.userlist
 
 openrc
--- srcs/nginx/srcs/install.sh
+++ srcs/nginx/srcs/install.sh
@@ -8,5 +8,9 @@ apk add openssh --no-cache # ssh
 # Request SSL key
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/C=FR/ST=75/L=Paris/O=42/CN=lmartin' -keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.crt
 
+# ssh
+useradd $SSH_USERNAME
+echo $SSH_PASSWORD | passwd $SSH_USERNAME --stdin
+
 # Update
 rc-update add sshd
