011a2dc01998d9f3e1ef6a1aaa643ea9b4122cc5
lmartin@student.42.fr <lmartin@e2r9p3.42.fr>
2020-02-07 03:57:16 +0100

    nginx working

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@
 #    By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/06 05:23:46 by lmartin           #+#    #+#              #
-#    Updated: 2020/02/06 05:46:55 by lmartin          ###   ########.fr        #
+#    Updated: 2020/02/07 03:35:57 by lmartin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
-
+# delete all
+kubectl delete -k ./srcs
 # ================================== VARIABLES =================================
 
 dir_goinfre=/goinfre/$USER/
@@ -22,25 +23,24 @@ eval $(minikube docker-env)
 # =================================== RESET ====================================
 
 # delete yaml
-kubectl delete -k ./srcs
-kubectl delete pvc wp-pv-claim
-kubectl delete pvc mysql-pv-claim
-kubectl delete pv wp-pv-volume
-kubectl delete pv mysql-pv-volume
+#kubectl delete pvc wp-pv-claim
+#kubectl delete pvc mysql-pv-claim
+#kubectl delete pv wp-pv-volume
+#kubectl delete pv mysql-pv-volume
 
 # delete previous images
 docker image rm -f nginx-server
 docker image rm -f ftps-server
-docker image rm -f wordpress-server
-docker image rm -f mysql-server
+#docker image rm -f wordpress-server
+#docker image rm -f mysql-server
 
 # ================================== LAUNCH ====================================
 
 # volume and volume claim
-kubectl apply -f $volumes/wp-pv-volume.yaml
-kubectl apply -f $volumes/mysql-pv-volume.yaml
-kubectl apply -f $volumes/wp-pv-claim.yaml
-kubectl apply -f $volumes/mysql-pv-claim.yaml
+#kubectl apply -f $volumes/wp-pv-volume.yaml
+#kubectl apply -f $volumes/mysql-pv-volume.yaml
+#kubectl apply -f $volumes/wp-pv-claim.yaml
+#kubectl apply -f $volumes/mysql-pv-claim.yaml
 
 # create path for archives if doesn't exist
 mkdir -p $dir_archive
@@ -48,17 +48,17 @@ mkdir -p $dir_archive
 # build archive
 docker build -t nginx-server srcs/nginx
 docker build -t ftps-server srcs/ftps
-docker build -t wordpress-server srcs/wordpress
-docker build -t mysql-server srcs/mysql
+#docker build -t wordpress-server srcs/wordpress
+#docker build -t mysql-server srcs/mysql
 # save to tar file
 docker save nginx-server > $dir_archive/nginx-server.tar
 docker save ftps-server > $dir_archive/ftps-server.tar
-docker save wordpress-server > $dir_archive/wordpress-server.tar
-docker save mysql-server > $dir_archive/mysql-server.tar
+#docker save wordpress-server > $dir_archive/wordpress-server.tar
+#docker save mysql-server > $dir_archive/mysql-server.tar
 # load from tar file
 docker load < $dir_archive/nginx-server.tar
 docker load < $dir_archive/ftps-server.tar
-docker load < $dir_archive/wordpress-server.tar
-docker load < $dir_archive/mysql-server.tar
+#docker load < $dir_archive/wordpress-server.tar
+#docker load < $dir_archive/mysql-server.tar
 # Call kustomization
 kubectl apply -k ./srcs
--- srcs/nginx/srcs/install.sh
+++ srcs/nginx/srcs/install.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+
 
 # Install
-apk add openrc
-apk add openssh
+apk add openrc --no-cache # rc-service
+apk add openssl --no-cache # Certificat SSL
+apk add openssh --no-cache # ssh
+
+# Request SSL key
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/C=FR/ST=75/L=Paris/O=42/CN=lmartin' -keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.crt
 
 # Update
 rc-update add sshd
