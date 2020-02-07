7a0a7fbd29300942c6528a53dbed7885426401c3
ska42 <lmartin@e2r9p2.42.fr>
2020-02-08 00:08:29 +0100

    fuck vsftpd, long life to pure-ftpd

--- srcs/ftps/srcs/install.sh
+++ srcs/ftps/srcs/install.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-# Install rc-service & vsftpd
-apk add openrc --no-cache
-apk add vsftpd --no-cache
+apk upgrade
 
-# create ftp's folders
-mkdir -p /var/empty
-mkdir -p /home/ftp
-chown nobody:nogroup /home/ftp
+# Install rc-service & pure-ftpd & ssl
+apk add openrc --no-cache
+apk add openssl --no-cache
+apk add pure-ftpd --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
+
+# ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem \
+-subj "/C=FR/ST=Anywhere/L=Here/O=42/OU=42/CN=lmartin/emailAddress=lmartin@student.42.fr"
+
+# create user
+adduser $FTP_USERNAME
+echo "$FTP_USERNAME:$FTP_PASSWORD" | chpasswd 
 
 # userlist
-echo "$FTP_USERNAME\n$FTP_PASSWORD" >> /etc/vsftpd.userlist
 
+openrc
 touch /run/openrc/softlevel
